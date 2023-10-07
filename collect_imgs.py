5ba3adebc93834e5ef37c5352d530f9377cc215a
Abilaash <39958409+Abilaash01@users.noreply.github.com>
2023-10-07 00:16:33 -0400

    Update collect_imgs.py

--- collect_imgs.py
+++ collect_imgs.py
@@ -18,7 +18,7 @@ for j in range(number_of_classes):
 
     done = False
     while True:
-        ret, frame = cap.read()
+        rect, frame = cap.read()
         cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
                     cv2.LINE_AA)
         cv2.imshow('frame', frame)
@@ -27,7 +27,7 @@ for j in range(number_of_classes):
 
     counter = 0
     while counter < dataset_size:
-        ret, frame = cap.read()
+        rect, frame = cap.read()
         cv2.imshow('frame', frame)
         cv2.waitKey(25)
         cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
