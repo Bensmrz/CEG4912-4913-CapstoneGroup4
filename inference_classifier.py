ebe6bf597140e3c25ec0036d5e6eac7a7185fbe8
Abilaash <39958409+Abilaash01@users.noreply.github.com>
2023-10-19 04:01:09 -0400

    Update inference_classifier.py

--- inference_classifier.py
+++ inference_classifier.py
@@ -13,7 +13,9 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I'} #edit based on number of labels/data being trained
+labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K',
+               11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U',
+               21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'} #edit based on number of labels/data being trained
 
 while True:
     data_aux = []
@@ -59,7 +61,7 @@ while True:
         predicted_character = labels_dict[int(prediction[0])]
 
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
-        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3, cv2.LINE_AA)
+        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_PLAIN, 1.3, (0, 0, 0), 3, cv2.LINE_AA)
 
     cv2.imshow('frame', frame)
     cv2.waitKey(1)
