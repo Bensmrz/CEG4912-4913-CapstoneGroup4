f13fe3b78ffecf7de39d2e1e77b0f33a8061771c
ImCalebP <91161976+ImCalebP@users.noreply.github.com>
2023-11-07 22:21:37 -0500

    Add files via upload
    Settings screen updated language button

--- AI-Glasses-App/pages/SettingsScreen.js
+++ AI-Glasses-App/pages/SettingsScreen.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
-import { View, Text, Switch, TextInput, Button } from 'react-native';
+import { View, Text, Switch, TouchableOpacity, StyleSheet } from 'react-native';
 
 function SettingsScreen() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [selectedLanguage, setSelectedLanguage] = useState('English');
-  const [currentPassword, setCurrentPassword] = useState('');
-  const [newPassword, setNewPassword] = useState('');
   const [notificationsEnabled, setNotificationsEnabled] = useState(true);
+
+  const changeLanguage = (language) => {
+    setSelectedLanguage(language);
+  };
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
   return (
     <View style={{ flex: 1, padding: 16 }}>
@@ -15,45 +21,34 @@ function SettingsScreen() {
       <Text style={{ fontSize: 16, marginBottom: 8 }}>Theme</Text>
       <Switch
         value={isDarkMode}
-        onValueChange={(value) => setIsDarkMode(value)}
+        onValueChange={() => toggleTheme()}
       />
+
+      <Text style={styles.themeText}>
+        {isDarkMode ? 'Dark Mode' : 'Light Mode'}
+      </Text>
 
       <Text style={{ fontSize: 16, marginBottom: 8 }}>Language</Text>
-      <TextInput
-        style={{ borderWidth: 1, padding: 8, marginBottom: 16 }}
-        value={selectedLanguage}
-        onChangeText={(text) => setSelectedLanguage(text)}
-      />
-
-      <Text style={{ fontSize: 16, marginBottom: 8 }}>Change Password</Text>
-      <TextInput
-        style={{ borderWidth: 1, padding: 8, marginBottom: 8 }}
-        placeholder="Current Password"
-        value={currentPassword}
-        onChangeText={(text) => setCurrentPassword(text)}
-        secureTextEntry
-      />
-      <TextInput
-        style={{ borderWidth: 1, padding: 8, marginBottom: 16 }}
-        placeholder="New Password"
-        value={newPassword}
-        onChangeText={(text) => setNewPassword(text)}
-        secureTextEntry
-      />
-
-<Button
-  title="Change Password"
-  onPress={() => {
-    
-    if (currentPassword === '' || newPassword === '') {
-      /
-      console.log('Please fill in both current and new passwords.');
-    } else {
-      
-    }
-  }}
-/>
-
+      <View style={{ flexDirection: 'row', marginBottom: 16 }}>
+        <TouchableOpacity
+          style={[
+            styles.languageButton,
+            selectedLanguage === 'English' && styles.selectedButton,
+          ]}
+          onPress={() => changeLanguage('English')}
+        >
+          <Text style={styles.buttonText}>English</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.languageButton,
+            selectedLanguage === 'French' && styles.selectedButton,
+          ]}
+          onPress={() => changeLanguage('French')}
+        >
+          <Text style={styles.buttonText}>French</Text>
+        </TouchableOpacity>
+      </View>
 
       <Text style={{ fontSize: 16, marginBottom: 8 }}>Notifications</Text>
       <Switch
@@ -63,5 +58,24 @@ function SettingsScreen() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  themeText: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  languageButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 8,
+    margin: 4,
+  },
+  selectedButton: {
+    backgroundColor: 'darkblue',
+  },
+  buttonText: {
+    color: 'white',
+  },
+});
 
 export default SettingsScreen;
