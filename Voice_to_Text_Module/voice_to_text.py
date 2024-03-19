3cf1424fd6d433c19cffd4e2e5855e2d9f149b4b
Bensmrz <55411938+Bensmrz@users.noreply.github.com>
2024-03-19 08:43:15 -0400

    Improved and completed speech to text

--- Voice_to_Text_Module/voice_to_text.py
+++ Voice_to_Text_Module/voice_to_text.py
@@ -1,28 +1,23 @@
-#must install speech recog extension in bash with pip install SpeechRecognition
-
+import time
 import speech_recognition as sr
 
-name = 'Ben'
+def listen_microphone():
+    recognizer = sr.Recognizer()
 
-def voice_to_text():
-
-    recognizer = sr.Recognizer() #Recognizer instance
-
-    with sr.Microphone() as source: #This is our input driver to capture audio
-
-        print("Hello! My name is " + name + ' and I am ready to help you!')
+    while True:
+        with sr.Microphone() as source:
+            print("DEFAULT NAME")
+            recognizer.adjust_for_ambient_noise(source)
+            audio = recognizer.listen(source)
 
         try:
-            audio_data = recognize.listen(source, timeout=30)
-            #print('Debug: Audio Captured Successfully')
-
-            text = recognizer.recognize.google(audio_data) #Google API for Speech recog
-            print(f"Text: {text}")
-        
-        #debugging
-        except sr.WaitTimeoutError:
-            print('Debug: No audio detected')
+            text = recognizer.recognize_google(audio)
+            print(text)
+            time.sleep(3)
         except sr.UnknownValueError:
-            print('Debug: Speech recognition could not understand audio')
+            print("DEFAULT NAME")
         except sr.RequestError as e:
-            print(f'Debug: Could not collect results from Google API')
+            print("Could not complete reques; {0}".format(e))
+
+if __name__ == "__main__":
+    listen_microphone()
